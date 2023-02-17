1bc7374fc6d23399ff4ce25562394f9b7cdf35af
vanor-cardozo <vanor.fc@gmail.com>
2023-02-17 11:48:00 -0300

    feat: Mapeando dataCoffee para gerar cards

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -4,31 +4,47 @@ import {
   DivCardFooter,
   CardContainer,
   CartButton,
+  SelectButtons,
+  TagClassification,
+  TagsContainer,
+  TitleMenu,
 } from './styles'
 
-import imgCof from '../../../../assets/coffeeImages/coffee01.png'
-import { ShoppingCart } from 'phosphor-react'
+import { coffeeData } from '../../../../data'
+import { Minus, Plus, ShoppingCart } from 'phosphor-react'
 
 export function CoffeeCard() {
   return (
-    <CardsContainer>
-      <CardContainer>
-        <DivCardHeader>
-          <img src={imgCof} />
-          <p>TRADICIONAL</p>
-          <p>EXPRESSO TRADICIONAL</p>
-          <p>O tradicional café feito com água quente e grãos moídos</p>
-        </DivCardHeader>
-        <DivCardFooter>
-          <p>R$ 9,90</p>
-          <label>
-            <input type="number" />
-          </label>
-          <CartButton>
-            <ShoppingCart size={22} weight="fill" />
-          </CartButton>
-        </DivCardFooter>
-      </CardContainer>
-    </CardsContainer>
+    <>
+      <CardsContainer>
+        <TitleMenu>Nossos cafés</TitleMenu>
+        {coffeeData.map((coffee) => (
+          <CardContainer key={coffee.id}>
+            <DivCardHeader>
+              <img src={coffee.coffeeImage} alt={coffee.name} />
+              <TagsContainer>
+                {coffee.classification.map((item) => (
+                  <TagClassification key={item}>{item}</TagClassification>
+                ))}
+              </TagsContainer>
+              <h4>{coffee.name}</h4>
+              <p>{coffee.description}</p>
+              <DivCardFooter>
+                <h5>R$</h5>
+                <h4>{coffee.price.toFixed(2)}</h4>
+                <SelectButtons>
+                  <Minus size={22} />
+                  <h6>1</h6>
+                  <Plus size={22} />
+                </SelectButtons>
+                <CartButton>
+                  <ShoppingCart size={22} weight="fill" />
+                </CartButton>
+              </DivCardFooter>
+            </DivCardHeader>
+          </CardContainer>
+        ))}
+      </CardsContainer>
+    </>
   )
 }
--- src/pages/Home/components/CoffeeCard/styles.ts
+++ src/pages/Home/components/CoffeeCard/styles.ts
@@ -2,11 +2,28 @@ import styled from 'styled-components'
 
 export const CardsContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 2rem;
+`
+export const TitleMenu = styled.div`
+  display: flex;
+  width: 100%;
+  font-family: 'Baloo 2';
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 4rem;
 `
 
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  height: 310px;
+  width: 256px;
+  padding: 1rem;
+
+  border-radius: 8px 50px;
 
   background: ${(props) => props.theme['base-card']};
 `
@@ -15,8 +32,9 @@ export const DivCardHeader = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 310px;
-  width: 256px;
+  position: relative;
+  bottom: 2.5rem;
+
   text-align: center;
   gap: 1rem;
 
@@ -24,11 +42,77 @@ export const DivCardHeader = styled.div`
     width: 7.5rem;
     height: 7.5rem;
   }
+
+  h4 {
+    font-size: 1.25rem;
+    font-family: 'Baloo 2';
+    font-weight: 700;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  p {
+    font-size: 0.875rem;
+    font-family: 'Roboto';
+    color: ${(props) => props.theme['base-label']};
+  }
+`
+
+export const TagsContainer = styled.div`
+  display: flex;
+  gap: 0.35rem;
+`
+
+export const TagClassification = styled.div`
+  display: flex;
+  background: ${(props) => props.theme['yellow-light']};
+  border-radius: 50px;
+  padding: 0.35rem;
+  font-size: 0.625rem;
+  font-weight: 700;
+  color: ${(props) => props.theme['yellow-dark']};
 `
 
 export const DivCardFooter = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+
+  h5 {
+    font-size: 0.875rem;
+    font-family: 'Baloo 2';
+    font-weight: 700;
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    font-family: 'Baloo 2';
+    font-weight: 900;
+    color: ${(props) => props.theme['base-text']};
+  }
+`
+
+export const SelectButtons = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: 4.5rem;
+  height: 2.375rem;
+  background: ${(props) => props.theme['base-button']};
+  padding: 0.5rem;
+  border-radius: 8px;
+  border: none;
+
+  color: ${(props) => props.theme['purple-med']};
+  &:hover {
+    color: ${(props) => props.theme['purple-dark']};
+  }
+
+  h6 {
+    font-size: 1rem;
+    color: #333;
+  }
 `
 
 export const CartButton = styled.button`
