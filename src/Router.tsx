dcaee69908224938d5e66db558c15149b3ab0b5a
vanor-cardozo <vanor.fc@gmail.com>
2023-02-23 17:58:03 -0300

    feat: Rota para confirmar pedido

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
+import { Success } from './pages/Success'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
       </Route>
     </Routes>
   )
